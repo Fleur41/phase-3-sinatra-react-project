c298de88e4c68baed0efb82470a288102de90e38
Fleur41 <Muriithikings456@gmail.com>
2023-03-04 16:03:12 +0300

    Completed lab

---
+++ app/models/project_task.rb
@@ -0,0 +1,6 @@
+class ProjectTask < ActiveRecord::Base
+    belongs_to :user
+  
+    validates :title, presence: true
+  end
+  
---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+#user(has many tasks) inherits from activerecord.
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+  has_many :project_tasks
+
+  validates :username, presence: true, uniqueness: true
+  validates :password_hash, presence: true
+
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+    return user if BCrypt::Password.new(user.password_hash) == password
+    nil
+  end
+end

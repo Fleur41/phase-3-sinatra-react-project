f6a32a9c0766d787f09d1a3e5a9b1a00d0a6dc3b
Fleur41 <Muriithikings456@gmail.com>
2023-03-04 17:08:33 +0300

    faker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,49 +4,49 @@ class ApplicationController < Sinatra::Base
   # Add your routes here
 
   get '/' do
-    project_task = Project_task.all
-    project_task.to_json
+    task = Task.all
+    task.to_json
   end
   get '/users' do 
     user = User.all
     user.to_json
   end
-  get "/user/project_tasks/:id" do
-    project_task = User.find(params[:id]).project_tasks
-    project_task.to_json
+  get "/user/tasks/:id" do
+    task = User.find(params[:id]).tasks
+    task.to_json
   end
-  get "/user/project_tasks/:id/:date" do
-    # project_task = User.find(params[:id]).project_tasks
-    project_task = Project_task.where(date: params[:date], user_id: params[:id]).to_json
+  get "/user/tasks/:id/:date" do
+    # task = User.find(params[:id]).tasks
+    task = Task.where(date: params[:date], user_id: params[:id]).to_json
     # puts params[:date]
-    project_task.to_json
+    task.to_json
   end
-  get "/project_tasks/:id" do
-    project_task = Project_task.find(params[:id])
-    project_task.to_json
+  get "/tasks/:id" do
+    task = Task.find(params[:id])
+    task.to_json
   end
-  get "/user/:id/project_tasks/:completion_status" do
-  #   project_task = User.find(params[:id])
-    project_task = Project_task.where(completion_status: params[:completion_status], user_id: params[:id]).to_json
-    project_task.to_json
+  get "/user/:id/tasks/:completion_status" do
+  #   task = User.find(params[:id])
+    task = Task.where(completion_status: params[:completion_status], user_id: params[:id]).to_json
+    task.to_json
   end
 
-  patch "/project_tasks/:id" do
-    project_task = Project_task.find(params[:id])
-    project_task.update(
+  patch "/tasks/:id" do
+    task = Task.find(params[:id])
+    task.update(
       completion_status: params[:completion_status]
     )
-    project_task.to_json
+    task.to_json
   end
-  post "/project_tasks" do
-    project_task = Project_task.create(
+  post "/tasks" do
+    task = Task.create(
       name: params[:name],
       description: params[:description],
       completion_status: params[:completion_status],
       user_id: params[:user_id],
       due_date: params[:due_date]
     )
-    project_task.to_json
+    task.to_json
   end
   post '/login' do 
     user = User.find_by(

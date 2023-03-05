66dd4797baf9c675bb3705f185a9efa1cc8d10e5
Fleur41 <Muriithikings456@gmail.com>
2023-03-05 14:11:50 +0300

    Create users

--- db/migrate/20230305105700_create_project.rb
+++ db/migrate/20230305105700_create_project.rb
@@ -1,12 +1,5 @@
 class CreateProject < ActiveRecord::Migration[6.1]
   def change
-    create_table :tasks do |t|
-      t.string :name
-      t.string :description
-      t.string :completion_status
-      t.date :date
-      t.date :due_date
-      t.integer :user_id
-    end
+    
   end
 end
